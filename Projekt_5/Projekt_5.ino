1a4f5934f50b33423257fa8afc3a35752834c36c
Rene Schimmelpfennig <mail@reneschimmelpfennig.de>
2014-07-02 20:18:56 +0200

    added project 5

---
+++ Projekt_5/Projekt_5.ino
@@ -0,0 +1,18 @@
+int ledPins[] = { 7, 8, 9, 10, 11, 12, 13 };
+int delayTime = 200;
+
+void setup()
+{
+  for (int i = 0; i < 7; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 7; i++) {
+    int pin = ledPins[i];
+    digitalWrite(pin, HIGH);
+    delay(delayTime);
+    digitalWrite(pin, LOW);
+  }
+}
